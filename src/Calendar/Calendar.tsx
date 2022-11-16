7ca7f8141e0ec67a14f8c1862817799dfa0e1553
Nicolas <joelnicolass@gmail.com>
2022-11-16 16:18:34 -0300

    refactor structure

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,45 +1,12 @@
 import React from 'react';
-import CalendarPicker from 'react-native-calendar-picker';
-import useSyncCalendar from './hooks/useSyncCalendar';
-import {StylesProps} from './styles/Calendar.stylesheet';
+import SyncCalendars from './components/SyncCalendars';
 
 type Props = {};
 
 const Calendar = (props: Props) => {
-  const {
-    startCalendarOne,
-    endCalendarOne,
-    startCalendarTwo,
-    endCalendarTwo,
-    onDateChangeCalendarOne,
-    onMonthChangeCalendarOne,
-    onDateChangeCalendarTwo,
-    getInitialAndMinDateCalendarTwo,
-    maxDateCalendarTwo,
-  } = useSyncCalendar();
-
   return (
     <>
-      <CalendarPicker
-        {...StylesProps}
-        allowRangeSelection={true}
-        onDateChange={onDateChangeCalendarOne}
-        onMonthChange={onMonthChangeCalendarOne}
-        selectedStartDate={startCalendarOne}
-        selectedEndDate={endCalendarOne}
-      />
-
-      <CalendarPicker
-        {...StylesProps}
-        allowRangeSelection={true}
-        onDateChange={onDateChangeCalendarTwo}
-        initialDate={getInitialAndMinDateCalendarTwo()}
-        minDate={getInitialAndMinDateCalendarTwo()}
-        maxDate={maxDateCalendarTwo()}
-        restrictMonthNavigation={true}
-        selectedStartDate={startCalendarTwo}
-        selectedEndDate={endCalendarTwo}
-      />
+      <SyncCalendars />
     </>
   );
 };
