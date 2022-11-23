21501f4e6e80f3b636ce1acb6df776d338cccfdd
nico <joelnicolass@gmail.com>
2022-11-23 13:40:38 -0300

    custom hook for sync implemented

--- src/components/syncCalendars/SyncCalendars.tsx
+++ src/components/syncCalendars/SyncCalendars.tsx
@@ -1,21 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useLayoutEffect, useState} from 'react';
-import useSingleCalendar from './hooks/useSingleCalendar';
-import CalendarPicker, {
-  CustomDatesStylesFunc,
-} from 'react-native-calendar-picker';
+import React, {useEffect, useState} from 'react';
+import CalendarPicker from 'react-native-calendar-picker';
 import {ItemCalendar} from '../../models/itemCalendar.interface';
-import moment, {Moment} from 'moment';
-import {DateChangedCallback} from 'react-native-calendar-picker';
-import {SELECTION_DATE} from '../../types/selectionDate.type';
-import {
-  getNextMonth,
-  getFirstDayOfMonth,
-  getLastDayOfMonth,
-} from '../../helpers/date';
-import {useFivvyCalendarProvider} from '../../hooks/useFivvyCalendarProvider';
 import adapterItem from './adapter/adapterItem';
 import {CustomDateStyle} from '../../models/customDateStyle.interface';
+import useSyncCalendars from './hooks/useSyncCalendars';
 
 interface Props {
   items?: ItemCalendar[];
@@ -24,35 +13,24 @@ interface Props {
 }
 
 const SyncCalendars = ({items, onMonthChange, onSelectDate}: Props) => {
-  const {start, end, setStart, setEnd} = useFivvyCalendarProvider();
+  const {
+    initialDateOne,
+    initialDateTwo,
+    loading,
+    maxDateOne,
+    maxDateTwo,
+    minDateOne,
+    minDateTwo,
+    selectedEndDateOne,
+    selectedEndDateTwo,
+    selectedStartDateOne,
+    selectedStartDateTwo,
+    onDateChangeCalendarOne,
+    onDateChangeCalendarTwo,
+    onMonthChangeHandler,
+  } = useSyncCalendars(onSelectDate, onMonthChange);
+
   const [styles, setStyles] = useState<any>(undefined);
-  const [loading, setLoadgin] = useState<boolean>(false);
-
-  const {
-    initialDate: initialDateOne,
-    setInitialDate: setInitialDateOne,
-    minDate: minDateOne,
-    setMinDate: setMinDateOne,
-    maxDate: maxDateOne,
-    setMaxDate: setMaxDateOne,
-    selectedStartDate: selectedStartDateOne,
-    setSelectedStartDate: setSelectedStartDateOne,
-    selectedEndDate: selectedEndDateOne,
-    setSelectedEndDate: setSelectedEndDateOne,
-  } = useSingleCalendar();
-
-  const {
-    initialDate: initialDateTwo,
-    setInitialDate: setInitialDateTwo,
-    minDate: minDateTwo,
-    setMinDate: setMinDateTwo,
-    maxDate: maxDateTwo,
-    setMaxDate: setMaxDateTwo,
-    selectedStartDate: selectedStartDateTwo,
-    setSelectedStartDate: setSelectedStartDateTwo,
-    selectedEndDate: selectedEndDateTwo,
-    setSelectedEndDate: setSelectedEndDateTwo,
-  } = useSingleCalendar();
 
   useEffect(() => {
     if (items) {
@@ -63,160 +41,6 @@ const SyncCalendars = ({items, onMonthChange, onSelectDate}: Props) => {
       setStyles(customStyles);
     }
   }, [items]);
-
-  // initial date sync
-  const initialDateHandler = (initialDate?: Date) => {
-    const today = initialDate || new Date();
-    const nextMonth = getNextMonth(today);
-    setInitialDateOne(today);
-    setInitialDateTwo(nextMonth);
-  };
-
-  const minDateHanlder = (date?: Date) => {
-    const nextMonth = getNextMonth(date || new Date());
-    const nextMonthFirstDay = getFirstDayOfMonth(nextMonth);
-    setMinDateTwo(nextMonthFirstDay);
-  };
-
-  const maxDateHanlder = (date?: Date) => {
-    const nextMonth = getNextMonth(date || new Date());
-    const nextMonthLastDay = getLastDayOfMonth(nextMonth);
-    setMaxDateTwo(nextMonthLastDay);
-  };
-
-  const onMonthChangeHandler: DateChangedCallback = (
-    date: Moment,
-    type: SELECTION_DATE,
-  ) => {
-    initialDateHandler(date.toDate());
-    minDateHanlder(date.toDate());
-    maxDateHanlder(date.toDate());
-    onMonthChange && onMonthChange();
-  };
-
-  const initializeSyncCalendars = () => {
-    initialDateHandler();
-    minDateHanlder();
-    maxDateHanlder();
-  };
-
-  useEffect(() => {
-    initializeSyncCalendars();
-  }, []);
-
-  //
-
-  const onPressCalendarOne = (date: Moment) => {
-    setLoadgin(true);
-    // si no hay fecha de inicio, se setea la fecha seleccionada
-    if (!start && !end) {
-      setStart(date.toDate());
-      setSelectedStartDateOne(date.toDate());
-      setSelectedStartDateTwo(date.toDate());
-
-      if (onSelectDate) {
-        onSelectDate(date.toDate(), undefined);
-      }
-
-      return;
-    }
-
-    if (start && !end) {
-      // si hay fecha de inicio, se setea la fecha de fin
-      setEnd(date.toDate());
-      setSelectedEndDateOne(date.toDate());
-      setSelectedEndDateTwo(date.toDate());
-
-      if (onSelectDate) {
-        onSelectDate(start, date.toDate());
-      }
-
-      return;
-    }
-
-    if (start && end) {
-      setStart(undefined);
-      setEnd(undefined);
-      setSelectedStartDateOne(undefined);
-      setSelectedStartDateTwo(undefined);
-      setSelectedEndDateOne(undefined);
-      setSelectedEndDateTwo(undefined);
-
-      if (onSelectDate) {
-        onSelectDate(undefined, undefined);
-      }
-
-      return;
-    }
-  };
-
-  const onPressCalendarTwo = (date: Moment) => {
-    setLoadgin(true);
-
-    if (!start && !end) {
-      // mover el calendario 1 a la fecha seleccionada
-      initialDateHandler(date.toDate());
-      minDateHanlder(date.toDate());
-      maxDateHanlder(date.toDate());
-      setStart(date.toDate());
-      setSelectedStartDateOne(date.toDate());
-      setSelectedStartDateTwo(date.toDate());
-
-      if (onSelectDate) {
-        onSelectDate(date.toDate(), undefined);
-      }
-
-      return;
-    }
-
-    if (start && !end) {
-      // si hay fecha de inicio, se setea la fecha de fin
-      setEnd(date.toDate());
-      setSelectedEndDateTwo(date.toDate());
-      setSelectedEndDateOne(date.toDate());
-
-      if (onSelectDate) {
-        onSelectDate(start, date.toDate());
-      }
-
-      return;
-    }
-
-    if (start && end) {
-      setStart(undefined);
-      setEnd(undefined);
-      setSelectedStartDateOne(undefined);
-      setSelectedStartDateTwo(undefined);
-      setSelectedEndDateOne(undefined);
-      setSelectedEndDateTwo(undefined);
-
-      if (onSelectDate) {
-        onSelectDate(undefined, undefined);
-      }
-
-      return;
-    }
-  };
-
-  const onDateChangeCalendarOne: DateChangedCallback = (
-    date: Moment,
-    type: SELECTION_DATE,
-  ) => {
-    onPressCalendarOne(date);
-  };
-
-  const onDateChangeCalendarTwo: DateChangedCallback = (
-    date: Moment,
-    type: SELECTION_DATE,
-  ) => {
-    onPressCalendarTwo(date);
-  };
-
-  useEffect(() => {
-    if (loading) {
-      setLoadgin(false);
-    }
-  }, [loading]);
 
   return (
     <>
--- src/components/syncCalendars/hooks/useSyncCalendars.ts
+++ src/components/syncCalendars/hooks/useSyncCalendars.ts
@@ -1,5 +1,222 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useFivvyCalendarProvider} from '../../../hooks/useFivvyCalendarProvider';
+import useSingleCalendar from './useSingleCalendar';
+import {
+  getNextMonth,
+  getFirstDayOfMonth,
+  getLastDayOfMonth,
+} from '../../../helpers/date';
+import {DateChangedCallback} from 'react-native-calendar-picker';
+import {Moment} from 'moment';
+import {SELECTION_DATE} from '../../../types/selectionDate.type';
 
-const useSyncCalendars = () => {};
+const useSyncCalendars = (
+  onSelectDate?: (
+    start: Date | undefined,
+    end: Date | undefined,
+  ) => void | undefined,
+  onMonthChange?: (() => void) | undefined,
+) => {
+  const {start, end, setStart, setEnd} = useFivvyCalendarProvider();
+
+  const [loading, setLoadgin] = useState<boolean>(false);
+
+  const {
+    initialDate: initialDateOne,
+    setInitialDate: setInitialDateOne,
+    minDate: minDateOne,
+    setMinDate: setMinDateOne,
+    maxDate: maxDateOne,
+    setMaxDate: setMaxDateOne,
+    selectedStartDate: selectedStartDateOne,
+    setSelectedStartDate: setSelectedStartDateOne,
+    selectedEndDate: selectedEndDateOne,
+    setSelectedEndDate: setSelectedEndDateOne,
+  } = useSingleCalendar();
+
+  const {
+    initialDate: initialDateTwo,
+    setInitialDate: setInitialDateTwo,
+    minDate: minDateTwo,
+    setMinDate: setMinDateTwo,
+    maxDate: maxDateTwo,
+    setMaxDate: setMaxDateTwo,
+    selectedStartDate: selectedStartDateTwo,
+    setSelectedStartDate: setSelectedStartDateTwo,
+    selectedEndDate: selectedEndDateTwo,
+    setSelectedEndDate: setSelectedEndDateTwo,
+  } = useSingleCalendar();
+
+  // initial date sync
+  const initialDateHandler = (initialDate?: Date) => {
+    const today = initialDate || new Date();
+    const nextMonth = getNextMonth(today);
+    setInitialDateOne(today);
+    setInitialDateTwo(nextMonth);
+  };
+
+  const minDateHanlder = (date?: Date) => {
+    const nextMonth = getNextMonth(date || new Date());
+    const nextMonthFirstDay = getFirstDayOfMonth(nextMonth);
+    setMinDateTwo(nextMonthFirstDay);
+  };
+
+  const maxDateHanlder = (date?: Date) => {
+    const nextMonth = getNextMonth(date || new Date());
+    const nextMonthLastDay = getLastDayOfMonth(nextMonth);
+    setMaxDateTwo(nextMonthLastDay);
+  };
+
+  const onMonthChangeHandler: DateChangedCallback = (
+    date: Moment,
+    type: SELECTION_DATE,
+  ) => {
+    initialDateHandler(date.toDate());
+    minDateHanlder(date.toDate());
+    maxDateHanlder(date.toDate());
+    onMonthChange && onMonthChange();
+  };
+
+  const initializeSyncCalendars = () => {
+    initialDateHandler();
+    minDateHanlder();
+    maxDateHanlder();
+  };
+
+  useEffect(() => {
+    initializeSyncCalendars();
+  }, []);
+
+  //
+
+  const onPressCalendarOne = (date: Moment) => {
+    setLoadgin(true);
+    // si no hay fecha de inicio, se setea la fecha seleccionada
+    if (!start && !end) {
+      setStart(date.toDate());
+      setSelectedStartDateOne(date.toDate());
+      setSelectedStartDateTwo(date.toDate());
+
+      if (onSelectDate) {
+        onSelectDate(date.toDate(), undefined);
+      }
+
+      return;
+    }
+
+    if (start && !end) {
+      // si hay fecha de inicio, se setea la fecha de fin
+      setEnd(date.toDate());
+      setSelectedEndDateOne(date.toDate());
+      setSelectedEndDateTwo(date.toDate());
+
+      if (onSelectDate) {
+        onSelectDate(start, date.toDate());
+      }
+
+      return;
+    }
+
+    if (start && end) {
+      setStart(undefined);
+      setEnd(undefined);
+      setSelectedStartDateOne(undefined);
+      setSelectedStartDateTwo(undefined);
+      setSelectedEndDateOne(undefined);
+      setSelectedEndDateTwo(undefined);
+
+      if (onSelectDate) {
+        onSelectDate(undefined, undefined);
+      }
+
+      return;
+    }
+  };
+
+  const onPressCalendarTwo = (date: Moment) => {
+    setLoadgin(true);
+
+    if (!start && !end) {
+      // mover el calendario 1 a la fecha seleccionada
+      initialDateHandler(date.toDate());
+      minDateHanlder(date.toDate());
+      maxDateHanlder(date.toDate());
+      setStart(date.toDate());
+      setSelectedStartDateOne(date.toDate());
+      setSelectedStartDateTwo(date.toDate());
+
+      if (onSelectDate) {
+        onSelectDate(date.toDate(), undefined);
+      }
+
+      return;
+    }
+
+    if (start && !end) {
+      // si hay fecha de inicio, se setea la fecha de fin
+      setEnd(date.toDate());
+      setSelectedEndDateTwo(date.toDate());
+      setSelectedEndDateOne(date.toDate());
+
+      if (onSelectDate) {
+        onSelectDate(start, date.toDate());
+      }
+
+      return;
+    }
+
+    if (start && end) {
+      setStart(undefined);
+      setEnd(undefined);
+      setSelectedStartDateOne(undefined);
+      setSelectedStartDateTwo(undefined);
+      setSelectedEndDateOne(undefined);
+      setSelectedEndDateTwo(undefined);
+
+      if (onSelectDate) {
+        onSelectDate(undefined, undefined);
+      }
+
+      return;
+    }
+  };
+
+  const onDateChangeCalendarOne: DateChangedCallback = (
+    date: Moment,
+    type: SELECTION_DATE,
+  ) => {
+    onPressCalendarOne(date);
+  };
+
+  const onDateChangeCalendarTwo: DateChangedCallback = (
+    date: Moment,
+    type: SELECTION_DATE,
+  ) => {
+    onPressCalendarTwo(date);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      setLoadgin(false);
+    }
+  }, [loading]);
+
+  return {
+    initialDateOne,
+    initialDateTwo,
+    minDateOne,
+    minDateTwo,
+    maxDateOne,
+    maxDateTwo,
+    selectedStartDateOne,
+    selectedStartDateTwo,
+    selectedEndDateOne,
+    selectedEndDateTwo,
+    onMonthChangeHandler,
+    onDateChangeCalendarOne,
+    onDateChangeCalendarTwo,
+    loading,
+  };
+};
 
 export default useSyncCalendars;
